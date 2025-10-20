37413824c60659b4be3e22d9ff2539167d16ba8b
beatricebulugu <151132273+beatricebulugu@users.noreply.github.com>
2025-10-20 18:51:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,4 @@
 <?php
-$host = 'db';
-$db = 'appdb';
-$user = 'appuser';
-$pass = 'apppass'; // parola NOUĂ
-
-try {
-    $pdo = new PDO("mysql:host=$host;port=3306;dbname=$db;charset=utf8mb4", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "✅ Conexiunea la baza de date a reușit!";
-} catch (PDOException $e) {
-    echo "❌ Eroare conexiune: " . $e->getMessage();
-}
+header('Location: login.php');
+exit;
 ?>
